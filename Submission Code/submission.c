397858619d9a844c431ee0ab5b1ef3b5c094622d
MultiDulcetia <155560738+MultiDulcetia@users.noreply.github.com>
2024-01-16 02:06:52 +0800

    Update submission.c

--- Submission Code/submission.c
+++ Submission Code/submission.c
@@ -1,24 +1,45 @@
-// Tic Tac Toe
+/*
+DCP5101 Program Design @ Multimedia University
+Trimester 2310 - 2023/24
+Assignment Topic: Tic-Tac-Toe Minigame
+Group Members: 1. @MultiDulcetia
+               2. @KahHeng0401
+               3. @riceo180
+               4. @ygjiaa
+*/ 
 
 #include <stdio.h>
 #include <stdlib.h> 
 #include <windows.h>
 
-// Print Tic Tac Toe Board
+// Print Tic-Tac-Toe Board
 void printBoard(char board[3][3])
 {
     printf("\n");
     for (int i = 0; i < 3; i++)
     {   
-            
+        printf(" %c | %c | %c ", board[i][0], board[i][1], board[i][2]);
+        if (i != 2)
+        {
+            printf("\n---|---|---\n");
+        }
     }
     printf("\n");
 }
+
+// Check Win
+int CheckWin(
+
+
+
+)
+
 
 int main()
 {
 
 
 
+    return 0;
 }
     
