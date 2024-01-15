9916de3ee98aca76a28e499aa5167a7dee70f052
MultiDulcetia <155560738+MultiDulcetia@users.noreply.github.com>
2024-01-15 10:52:40 +0800

    Create submission.c

---
+++ Submission Code/submission.c
@@ -0,0 +1,24 @@
+// Tic Tac Toe
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <windows.h>
+
+// Print Tic Tac Toe Board
+void printBoard(char board[3][3])
+{
+    printf("\n");
+    for (int i = 0; i < 3; i++)
+    {   
+            
+    }
+    printf("\n");
+}
+
+int main()
+{
+
+
+
+}
+    
