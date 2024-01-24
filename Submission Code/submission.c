dd44e333363077db8151f555c9a5997c74269edd
MultiDulcetia <155560738+MultiDulcetia@users.noreply.github.com>
2024-01-24 17:21:52 +0800

    update submission.c & recompiled & create a leaderboard.txt

--- Submission Code/submission.c
+++ Submission Code/submission.c
@@ -21,8 +21,9 @@ You have to apply at least 5 elements in your program. Elements that you can app
 
 //Preprocessor directives
 #include <stdio.h>
+// Using <stdlib.h> so that we can use the exit() function to exit the program
 #include <stdlib.h>
-// Using windows.h so that we can use the sleep() function to autoclose after 5 sec and not have the cmd auto close
+// Using windows.h so that we can use the sleep() function to autoclose after 5 sec and not have the command prompt auto close
 #include <windows.h>
 
 // Defining the size of the board which is 9x9
@@ -102,25 +103,39 @@ int main()
         char leaderboard_string[10000];
 
         printf("\nViewing leaderboard...\n");
-        // opening file "leaderboard.txt"
         file = fopen("leaderboard.txt", "r");
-        // if file doesn't exist, print error msg and quit
+        
+        // if file does not exist, print error and exit program
         if (file == NULL)
         {
             printf("Error opening file!\n");
-            printf("Exiting in 5 seconds...\n\n");
+            printf("Exiting in 5 seconds...\n");
             Sleep(5000);
             exit(1);
 
 
-        }else // else print leaderboard
+        }else 
+        
+        printf("\n------------------------------------");
+        printf("\n        Start of Leaderboard        ");
+        printf("\n------------------------------------\n");
+        do // do loop to keep printing until end of file
         {
-        fgets(leaderboard_string, 10000, file);
-        printf("%s\n", leaderboard_string);
-    
-        fclose(file);
-        }
+            fgets(leaderboard_string, 10000, file);
+            printf("%s", leaderboard_string);
+        } while (!feof(file)); // feof = file end of file
+        printf("\n------------------------------------");
+        printf("\n         End of Leaderboard         ");
+        printf("\n------------------------------------");
+        
+        
+        // fgets(leaderboard_string, 10000, file);
+        // printf("%s\n", leaderboard_string);
+        
 
+
+
+        fclose(file); // close file to prevent memory leak
     }
     else if (main_menu_choice == 3)
     {
@@ -132,6 +147,15 @@ int main()
         printf("Invalid input! Please try again.\n");
     }
     
+    
+
+
+
+
+
+
+
+
     // pause for 5 sec before closing
     Sleep(5000);
     return 0;
